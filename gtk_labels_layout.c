01a0365bc7afab69fcb26115cb4993705121fab6
Cesar <cesarli619@gmail.com>
2025-05-04 23:02:12 -0400

    Add label and button to box, then add box to window

--- gtk_labels_layout.c
+++ gtk_labels_layout.c
@@ -19,14 +19,17 @@ int main(int argc, char *argv[])
     // Below makes X button close program correctly.
     g_signal_connect(win, "delete_event", G_CALLBACK(end_program), NULL);
 
-    // GtkWindow is a child of GtkContainer
-    // Cast win to GtkContainer
-    gtk_container_add(GTK_CONTAINER (win), btn);
-
-    // Code for label
     GtkWidget *lbl = gtk_label_new("My label");
-    gtk_container_add(GTK_CONTAINER (win), lbl);
 
+    // Using a Box widget to put window with button and label
+    // since a window can only have 1 widget.
+    // gtk_box_new(orientation, spacing) - Orientation 0 - Hor, 1 - Ver
+    GtkWidget *box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
+    gtk_box_pack_start(GTK_BOX(box), lbl, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(box), btn, TRUE, TRUE, 0);
+
+    // Then add Box to Win
+    gtk_container_add(GTK_CONTAINER(win), box);
     gtk_widget_show_all(win);
     gtk_main();
     return 0;
